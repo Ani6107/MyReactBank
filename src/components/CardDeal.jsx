64eed01b2e1078cb4ed36bc60e17bb6bb0901807
ANIRUDDH ADITYA SHARMA <89570884+Ani6107@users.noreply.github.com>
2023-05-29 12:28:12 +0530

    Add files via upload

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,27 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Evaluate your current card and identify your requirements.
+      Research and compare different credit card options, considering factors like interest rates, fees, and rewards programs.
+      Apply for the chosen card and monitor your credit to ensure accuracy and take advantage of its benefits.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
